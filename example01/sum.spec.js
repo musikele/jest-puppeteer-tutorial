120b4d9b51c27acf8defc89e4edf75b10c07cb05
Michele Nasti <michele.nasti@gmail.com>
2019-09-22 23:16:58 +0200

    dom manipulation example

--- example01/sum.spec.js
+++ example01/sum.spec.js
@@ -2,15 +2,14 @@ const sum = require('./sum')
 
 describe('sum module', () => {
     it('should sum two numbers', () => {
-        const result = sum(2,3);
-        expect(result).toBe(5);
+        const result = sum(2,3)
+        expect(result).toBe(5)
     });
 
     it('should explode with exception', () => {
         try {
             sum(2,'pippo');
         } catch(e) {
-            console.log(e.message);
             expect(e).toBeDefined();
         }
     });
